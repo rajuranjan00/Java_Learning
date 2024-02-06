fc5c065a0ca487b495b5bf528ad8b797d575666a
rajuranjan00 <rajuranjan144k@gmail.com>
2024-02-06 08:26:08 +0530

    Realtional operator theory added

--- Relational_Operator.java
+++ Relational_Operator.java
@@ -20,3 +20,21 @@ public class Relational_Operator {
 	}
 
 }
+
+/*
+ * Relational operators serve the purpose of comparing values in programming. This includes using operators such as less than, greater than, and equality checks between two values. 
+ * In Java, the assignment operator (=) assigns values, as demonstrated in the example "x=5," where the value 5 is assigned to the variable x.
+
+To compare values, the double equal to (==) operator is utilized. Conversely, the not equal to operator (!=) is employed when checking for inequality between two values.
+
+The set of relational operators comprises:
+
+Less than
+Greater than
+Equal to (for comparison)
+Not equal to
+Less than or equal to
+Greater than or equal to
+It is essential to note that relational operators yield a boolean result, indicating either true or false based on the outcome of the comparison. 
+They find frequent application in loops and conditional statements within programming logic.
+ */
